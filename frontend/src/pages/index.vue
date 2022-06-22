dccd7542064b5d2883349c04e4ccd9af475f60ee
JDN89 <jan.de.niels@gmail.com>
2022-06-22 18:57:35 -0500

    feat: display books

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import type { Book } from 'types'
+let books = $ref<Book[]>()
+onMounted(async () => {
+  books = await eventService.getAllBooks().then(response => response.data)
+})
 /* const name = $ref('')
 
 const router = useRouter()
@@ -9,5 +14,18 @@ const go = () => {
 </script>
 
 <template>
-  <div class="flex" />
+  <div class="flex ">
+    //flex the v-for loop vertical
+    <div v-for="book in books" :key="book.id" class=" m2">
+      <div id="container" class="">
+        <div id="author" class="p-2 border-1 border:gray/20">
+          {{ book.author }}
+          {{ book.pages }}
+        </div>
+        <div id="pages" class="p-2">
+          {{ book.pages }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
